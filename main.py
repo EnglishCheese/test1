e7811960fae1a164ebc86d8de29800fd258c9013
EnglishCheese <145911165+EnglishCheese@users.noreply.github.com>
2023-09-24 17:25:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,16 +14,25 @@ text2 = QLabel(win)
 text2.setText('?')
 text2.move(190, 70)
 
+text3 = QLabel(win)
+text3.setText('?')
+text3.move(210, 70)
+
 button = QPushButton(win)
 button.setText("Згенерувати")
 button.move(250, 70)
 
+
 def show_win():
-    number = randint(1, 100)
+    number = randint(9,20)
+    number1 = randint(1, 9)
     text2.setText(str(number))
+    text3.setText(str(number1))
     text.setText('Переможець:')
+
 
 button.clicked.connect(show_win)
 
 win.show()
 app.exec_()
+
